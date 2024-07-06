2b60ab66565eea9a4c0d49a8de2982abfb9bb6e8
nickoff <nick.olehnovich@gmail.com>
2024-07-06 19:17:05 +0300

    chore: set up esLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,10 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-compiler'],
   rules: {
+    'react-compiler/react-compiler': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
