40cf074cc23bbef9fc7164eeb4a994ebda9a8abd
nickoff <nick.olehnovich@gmail.com>
2024-07-06 20:28:37 +0300

    feat: add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<p>Ups.. Something went wrong!</p>}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
