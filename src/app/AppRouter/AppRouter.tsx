c0820b66f3ccd6fd2c98b3411bfa8cdd7c39c2c1
nickoff <nick.olehnovich@gmail.com>
2024-07-14 16:58:12 +0300

    feat: add NotFound page

--- src/app/AppRouter/AppRouter.tsx
+++ src/app/AppRouter/AppRouter.tsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, LoaderFunction, Route, RouterProvider } from 'react-router-dom';
 import { Main } from '../../views/Main/Main';
 import { PersonDescription } from '../../components/PersonDescription/PersonDescription';
+import { NotFound } from '../../views/NotFound/NotFound';
 
 const getPersonDescription = async (id: string | undefined) => {
   if (!id) return;
   const apiUrl = `https://swapi.dev/api/people/${id}`;
   const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data;
+  const personDescription = await response.json();
+  return { personDescription };
 };
 const loaderPersonDescription: LoaderFunction<{ personDescription: PersonDescription }> = async ({ params }) => {
-  const personDescription = await getPersonDescription(params.id);
-  return { personDescription };
+  return await getPersonDescription(params.id);
 };
 
 export const AppRouter = () => {
@@ -20,7 +20,7 @@ export const AppRouter = () => {
       <Route path="/" element={<Main />} errorElement={<p>Upps</p>}>
         <Route path="person/:id" loader={loaderPersonDescription} element={<PersonDescription />} />
       </Route>
-      <Route path="*" element={<p>404</p>} />
+      <Route path="*" element={<NotFound />} />
     </>
   );
   const router = createBrowserRouter(routers, {});
--- src/components/PersonDescription/PersonDescription.tsx
+++ src/components/PersonDescription/PersonDescription.tsx
@@ -10,10 +10,12 @@ export interface PersonDescription {
   eye_color: string;
   birth_year: string;
   gender: string;
+  detail?: boolean;
 }
 
 export const PersonDescription = () => {
   const { personDescription } = useLoaderData() as { personDescription: PersonDescription };
+  console.log(personDescription);
   const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender } = personDescription;
 
   return (
@@ -21,19 +23,25 @@ export const PersonDescription = () => {
       <Link className="person-description__back-button" to="/">
         CLOSE
       </Link>
-      <h3>{name}</h3>
-      <div>
-        <p>Person description:</p>
-        <ul>
-          <li>height: {height}</li>
-          <li>mass: {mass}</li>
-          <li>hair color: {hair_color}</li>
-          <li>skin color: {skin_color}</li>
-          <li>eye color: {eye_color}</li>
-          <li>birth year: {birth_year}</li>
-          <li>gender: {gender}</li>
-        </ul>
-      </div>
+
+      {personDescription.detail && <p className="person-description__not-found">Not found person</p>}
+      {personDescription.name && (
+        <>
+          <h3>{name}</h3>
+          <div>
+            <p>Person description:</p>
+            <ul>
+              <li>height: {height}</li>
+              <li>mass: {mass}</li>
+              <li>hair color: {hair_color}</li>
+              <li>skin color: {skin_color}</li>
+              <li>eye color: {eye_color}</li>
+              <li>birth year: {birth_year}</li>
+              <li>gender: {gender}</li>
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 };
