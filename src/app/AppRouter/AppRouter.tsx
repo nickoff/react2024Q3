5c8eaec2cdf0bd3383c38cbc6cd5f53a279a3074
nickoff <nick.olehnovich@gmail.com>
2024-07-21 19:11:50 +0300

    feat: add AppRouter test

--- src/app/AppRouter/AppRouter.tsx
+++ src/app/AppRouter/AppRouter.tsx
@@ -1,26 +1,9 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  LoaderFunction,
-  Navigate,
-  Route,
-  RouterProvider
-} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom';
 import { Main } from '../../views/Main/Main';
 import { PersonDescription } from '../../components/PersonDescription/PersonDescription';
 import { NotFound } from '../../views/NotFound/NotFound';
 import { Layout } from '../../views/Layout/Layout';
-
-const getPersonDescription = async (id: string | undefined) => {
-  if (!id) return;
-  const apiUrl = `https://swapi.dev/api/people/${id}`;
-  const response = await fetch(apiUrl);
-  const personDescription = await response.json();
-  return { personDescription };
-};
-const loaderPersonDescription: LoaderFunction<{ personDescription: PersonDescription }> = async ({ params }) => {
-  return await getPersonDescription(params.id);
-};
+import { loaderPersonDescription } from './api';
 
 export const AppRouter = () => {
   const routers = createRoutesFromElements(
