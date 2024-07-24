066bd235486cc85f569a335228b95c0655ab6acf
nickoff <nick.olehnovich@gmail.com>
2024-07-24 22:37:01 +0300

    chore: add redux and RTK

--- src/app/Providers/Providers.tsx
+++ src/app/Providers/Providers.tsx
@@ -1,5 +1,7 @@
 import { ErrorBoundary } from '../../components/ErrorBoundary/ErrorBoundary';
 import { Fallback } from '../../views/Fallback/Fallback';
+import { store } from '../../store';
+import { Provider } from 'react-redux';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -9,6 +11,8 @@ export const Providers = (props: ProvidersProps) => {
   const { children } = props;
 
   return (
-    <ErrorBoundary fallback={(handlerReload) => <Fallback reloadCallback={handlerReload} />}>{children}</ErrorBoundary>
+    <ErrorBoundary fallback={(handlerReload) => <Fallback reloadCallback={handlerReload} />}>
+      <Provider store={store}>{children}</Provider>
+    </ErrorBoundary>
   );
 };
